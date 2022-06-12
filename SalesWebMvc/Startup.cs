7132d3e66ebe4fb5e12bcd62084a90ca275611a3
matgil05 <matheus-gil@hotmail.com>
2022-06-12 18:19:23 -0300

    SellerService e basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -42,6 +43,7 @@ namespace SalesWebMvc
 
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
 
         }
 
